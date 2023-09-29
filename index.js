3ff04982a066d2d3b987121df2b5dd3026d7f5bf
fredtyfirst <fredtyfirst@yahoo.fr>
2023-09-29 02:04:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
+const contents = document.querySelectorAll('.content div');
 
 function showContent(tabNumber) {
-  const contents = document.querySelectorAll('.content div');
   contents.forEach(content => content.classList.remove('active'));
   document.getElementById(`content${tabNumber}`).classList.add('active');
 }
